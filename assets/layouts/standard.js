ee84652e78e80e6a561b8ecddb6d846f656dcec3
Manatsawin Hanmongkolchai <manatsawin@gmail.com>
2012-06-08 22:04:51 +0700

    Standard Layout: Transparent BG/dark

--- assets/layouts/standard.js
+++ assets/layouts/standard.js
@@ -5,14 +5,23 @@ layouts["standard"] = Layout.extend({
 		javascripts: [
 			"assets/jquery.js",
 		],
-		stylesheets: [
-			"files/standard.css"
-		],
+		stylesheets: function(){
+			var out = ["files/standard.css"];
+			try{
+				if(this.model.page.get("config_standard").transparentbg){
+					out.push("#container,.dark #container{background: transparent};\n");
+				}
+			}catch(e){}
+			return out;
+		},
 		resources: [
 			"bao.otf",
 			"supermarket.ttf",
 			"qleft.png",
 			"qright.png"
-		]
+		],
 	}),
+	config: TemplLayoutConfigView.extend({
+		template: "layout_standard",
+	})
 });
