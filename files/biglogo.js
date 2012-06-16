2151501010f3cb3f624e612d2d7bf485be98b1b7
Manatsawin Hanmongkolchai <manatsawin@gmail.com>
2012-06-16 13:55:19 +0700

    logo now have video support

--- files/biglogo.js
+++ files/biglogo.js
@@ -2,12 +2,20 @@ $(function(){
 	$(".biglogo img").load(function(){
 		$(window).resize();
 	});
+	$(".biglogo").css({
+		height: $(window).height(),
+		display: "table",
+		width: "100%"
+	});
+	$(".biglogo img").css({
+		maxWidth: "100%",
+		maxHeight: "100%"
+	});
+	$(".biglogo video").css({
+		width: "100%",
+		height: "100%"
+	});
 	$(window).resize(function(){
-		$(".biglogo").css({
-			height: $(window).height(),
-			display: "table",
-			width: "100%"
-		});
 		$(".biglogo").each(function(){
 			var position = $(this).data("position") || "mc";
 			if($(this).data("transparent")){
@@ -23,4 +31,21 @@ $(function(){
 			});
 		});
 	}).resize();
+	$("video").bind("canplaythrough", function(){
+		$(this).addClass("readyload");
+		if(isFocus){
+			$(this).addClass("playstarted");
+			$(this).get(0).play();
+		}
+	});
+	var isFocus = true;
+	$(window).blur(function(){
+		isFocus = false;
+	});
+	$(window).focus(function(){
+		isFocus = true;
+		$("video.readyload:not(.playstarted)").addClass("playstarted").each(function(){
+			this.play();
+		});
+	});
 });
