928e00142d7eb25c4867fd09526ab1d1a841f12d
Manatsawin Hanmongkolchai <manatsawin@gmail.com>
2012-06-18 20:57:13 +0700

    Editor talk widget

---
+++ assets/widgets/editortalk.js
@@ -0,0 +1,21 @@
+widgets["editortalk"] = Widget.extend({
+	type: "editortalk",
+	name: "Editor talk",
+	description: "Talks. Talks. Talks some more.",
+	config: TemplConfigView.extend({
+		template: "editortalk"
+	}),
+	renderer: Backbone.View.extend({
+		tagName: "div",
+		initialize: function(){
+			this.template = load_template("widgets", "editortalk");
+			this.model.on("change", function(){
+				this.render();
+			}, this);
+		},
+		render: function(){
+			this.el.innerHTML = this.template(this.model.toJSON());
+		},
+		stylesheets: ["files/editortalk.css"]
+	}),
+});
