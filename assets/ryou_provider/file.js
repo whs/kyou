c50f6377d8dfb1a81ebed1990d680a20de6e25c6
Manatsawin Hanmongkolchai <manatsawin@gmail.com>
2012-06-30 12:43:04 +0700

    minor bugfixes for ryou widget (lyric)

--- assets/ryou_provider/file.js
+++ assets/ryou_provider/file.js
@@ -8,7 +8,7 @@ if(window['inRyou'] || window['inKyou']){
 ryou_provider['file'] = RyouProvider.extend({
 	tagName: "audio",
 	render: function(){
-		if(!this.model.get("file").match(/:\/\/|^\//)){
+		if(!this.model.get("file").match(/:\/\/|^\//) && !window['inKyou']){
 			this.el.src = "/bookfiles/" + this.model.project.id + "/" + this.model.get("file");
 		}else{
 			this.el.src = this.model.get("file");
